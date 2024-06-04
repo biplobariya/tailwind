1562b5433da30a36ced9b80051dfabb67fe7a778
Biplob <biplobariya4@gmail.com>
2024-06-04 07:48:47 +0600

    working update

--- custom.js
+++ custom.js
@@ -1,3 +1,22 @@
+// Navbar Responsive JS
+var toggleOpen = document.getElementById('toggleOpen');
+var toggleClose = document.getElementById('toggleClose');
+var collapseMenu = document.getElementById('collapseMenu');
+
+function handleClick() {
+  if (collapseMenu.style.display === 'block') {
+    collapseMenu.style.display = 'none';
+  } else {
+    collapseMenu.style.display = 'block';
+  }
+}
+
+toggleOpen.addEventListener('click', handleClick);
+toggleClose.addEventListener('click', handleClick);
+
+
+
+
 
 //Initialize Swiper 
     var swiper = new Swiper(".hero-slider", {
